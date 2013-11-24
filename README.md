7fa159ca052cb56dfe5bf55d7f3c787a07665f5e
Mark J Pritt <mjpritt@masters2.cs.jhu.edu>
2013-11-24 16:39:30 -0500

    Insert almost working

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ fm-update
 =========
 
 Final project for Computational Genomics
+
+Uses an FM index to map reads to a reference genome. Continuously updates the FM index to match the genome from which reads were sequenced.
--- bwt.py
+++ bwt.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 import math
 import numpy as np
-
+import random
 
 def rotations(t):
     ''' Return list of rotations of input string t '''
@@ -17,14 +17,17 @@ def bwm(t, a):
     sort = sorted(rot2)
  
     bwm = [x for (x,_) in sort]
-    sa = [y for (_,y) in sort]
+#    sa_complete = [y for (_,y) in sort]
+    sa = dict()
+    for i in xrange(0, len(sort), a):
+        sa[i] = sort[i][1]
   
-    return sorted(rotations(t)), sa[(a-1)::a]
+    return sorted(rotations(t)), sa
 
 def bwtViaBwm(t,a):
     ''' Given T, returns BWT(T) and sampled SA(T) by way of the BWM '''
     bw, sa = bwm(t,a)
-    return ''.join(map(lambda x: x[-1], bw)), sa
+    return map(lambda x: x[-1], bw), sa
 
 def rankBwt(bw):
     ''' Given BWT string bw, return parallel list of B-ranks.  Also
@@ -63,11 +66,29 @@ def reverseBwt(bw):
 def getSAIndex(fm, i, a, alphabet):
     (first, last, sa, checkpoints) = fm
     ''' Returns the index in the full SA of the given index i in the bw transform, using the given subsampled SA '''
-    if (i+1) % a == 0:
-        return sa[(i+1)/a - 1]
+    if i in sa:
+        return sa[i]
     else:
         newi = first[last[i]][0] + getCount(last, i, checkpoints, b, alphabet)
         return (1 + getSAIndex(fm, newi, a, alphabet)) % len(last)
+
+def getRowBySA(fm, index, a, alphabet):
+    ''' Return the row  in the bwt with the given SA value '''
+    (first, last, sa, checkpoints) = fm
+
+    minK = 0
+    minDiff = len(last)
+    for k,v in sa.items():
+        if v == index:
+            return k
+        elif (v-index) > 0 and (v-index) < minDiff:
+            minK = k
+            minDiff = v-index
+
+    row = minK
+    for i in range(minDiff):
+       row = LF(fm, row)
+    return row
 
 def getCheckpoints(bw, b, alphabet):
     ''' Returns counts of each letter at every b positions in the bw string '''
@@ -84,12 +105,13 @@ def getCheckpoints(bw, b, alphabet):
     return checkpoints
    
 def getCount(bw, i, checkpoints, b, alphabet):
-    '''Find the b-index of the character at the given position in the burrows-wheeler transform''' 
+    '''Find the b-index of the character at the given position in the burrows-wheeler transform'''
     prevC = i - ((i+1) % b)
     nextC = prevC+b
+
     if i == prevC:
         return int(checkpoints[(i+1)/b - 1][alphabet.index(bw[i])]) - 1
-    elif prevC > -1 and (nextC > len(bw) or (i - prevC) < (nextC - i)):
+    elif prevC > -1 and (nextC >= len(bw) or (i - prevC) < (nextC - i)):
         count = int(checkpoints[(i+1)/b - 1][alphabet.index(bw[i])])
         for x in bw[prevC+1:i]:
             if x == bw[i]:
@@ -118,7 +140,7 @@ def findRange(fm, a, b, substring, start, end):
                 matches.append(getSAIndex(fm, i, a, alphabet))
             return sorted(matches)
         else:
-            minId = end-start
+            minId = len(last)
             maxId = 0
             for i in xrange(startId, endId):
                 if last[i] == substring[-2]:
@@ -132,20 +154,194 @@ def findRange(fm, a, b, substring, start, end):
             else:
                 return findRange(fm, a, b, substring[:-1], int(minId), int(maxId)+1)
 
+def moveRow(fm, i, j):
+    ''' Moves the row from i to j, shifting all rows in-between and updates SA and checkpoints '''
+    (first, last, sa, checkpoints) = fm
+    if i > j:
+        last = last[:j] + [last[i]] + last[j:i] + last[i+1:]
+
+        # Update SA
+        for k,v in sa.items():
+            if k >= j and k < i:
+                sa[k+1] = v
+                del sa[k]
+            elif k == i:
+                sa[j] = v
+                del sa[k]
+
+        # Loop through checkpoints between j and i
+        indexAdded = alphabet.index(last[j])
+        for x in xrange(j + b - (j%b+1), i, b):
+            checkpoints[(x+1) / b - 1][indexAdded] += 1
+            checkpoints[(x+1) / b - 1][alphabet.index(last[x+1])] -= 1
+    else:
+        last = last[:i] + last[i+1:j+1] + [last[i]] + last[j+1:]
+        
+        # Update SA
+        for k,v in sa.items():
+            if k > i and k <= j:
+                sa[k-1] = v
+                del sa[k]
+            elif k == i:
+                sa[j] = v
+                del sa[k]
+
+        # Loop through checkpoints between j and i
+        indexRemoved = alphabet.index(last[j])
+        for x in xrange(i + b - (i%b+1), j, b):
+            checkpoints[(x+1) / b - 1][indexRemoved] -= 1
+            checkpoints[(x+1) / b - 1][alphabet.index(last[x])] += 1
+    return (first, last, sa, checkpoints)
+
 def find(fm, a, b, substring):
     ''' Find all indexes of the substring in the text represented in bw '''
     first = fm[0]
     (minI, maxI) = first[substring[-1]]
     return findRange(fm, a, b, substring, 0, maxI-minI)
 
-def insert(index, c):
+def insert(fm, index, c):
     ''' Update the BWT for an insertion of character c into position index in the original string '''
+    (first, last, sa, checkpoints) = fm
+
+    # Update old row
+    row = getRowBySA(fm, index, a, alphabet)
+    tempC = last[row]
+    last[row] = c
+
+    # Update first column
+    for k in alphabet:
+        if k == c:
+            first[k] = (first[k][0], first[k][1]+1)
+        elif k > c:
+            first[k] = (first[k][0]+1, first[k][1]+1)
+
+    # Add new row
+    bVal = getCount(last, row, checkpoints, b, alphabet)
+    if not tempC == c:
+        bVal += 1
+
+    newRow = first[c][0] + bVal
+    last = last[:newRow] + [tempC] + last[newRow:]
 
 
+    # Update SA
+    for k,v in sa.items():
+        if k >= newRow:
+            if v >= index:
+                v += 1
+            del sa[k]
+            sa[k+1] = v
+        else:
+            if v >= index:
+                sa[k] = v+1
+
+    print checkpoints
+    # Add new checkpoint row
+    if len(last) % b == 0:
+        # WHY ISN'T THIS CLONING
+        newCheckpoint = checkpoints[len(checkpoints)-1][:]
+        for x in xrange(len(checkpoints)*b,len(last)):
+            newCheckpoint[alphabet.index(last[x])] += 1
+        checkpoints = np.vstack([checkpoints, newCheckpoint])
+        print checkpoints
+    # Update checkpoints
+    if row < newRow:
+        # Loop through checkpoints between row and newRow
+        indexAdded = alphabet.index(c)
+        indexRemoved = alphabet.index(tempC)
+        for x in xrange(row + b - (row%b+1), newRow, b):
+            checkpoints[(x+1) / b - 1][indexAdded] += 1
+            checkpoints[(x+1) / b - 1][indexRemoved] -= 1
+        for x in xrange(newRow + b - (newRow%b+1), len(last), b):
+            checkpoints[(x+1) / b - 1][indexAdded] += 1
+            checkpoints[(x+1) / b - 1][alphabet.index(last[x+1])] -= 1
+    else:
+        # Loop through checkpoints between row and newRow
+        indexAdded = alphabet.index(c)
+        indexRemoved = alphabet.index(tempC)
+        for x in xrange(newRow + b - (newRow%b+1), row, b):
+            checkpoints[(x+1) / b - 1][indexRemoved] += 1
+            checkpoints[(x+1) / b - 1][alphabet.index(last[x+1])] -= 1
+        for x in xrange((row+1) + b - ((row+1)%b+1), len(last), b):
+            checkpoints[(x+1) / b - 1][indexAdded] += 1
+            checkpoints[(x+1) / b - 1][alphabet.index(last[x+1])] -= 1
+    print checkpoints
+
+    
+
+    # Rearrange rows that are now out of order
+    fm = (first, last, sa, checkpoints)
+
+    for i in xrange(len(last)):
+        if ((i+1) % b == 0):
+            n = (i+1)/b-1
+            print last[i] + '\t' + str(getSAIndex(fm, i, a, alphabet)) + '\t' + str(checkpoints[n][0]) + '\t' + str(checkpoints[n][1]) + '\t' + str(checkpoints[n][2]) + '\t' + str(checkpoints[n][3]) + '\t' + str(checkpoints[n][4])
+        else:
+            print last[i] + '\t' + str(getSAIndex(fm, i, a, alphabet))
+    print sa
+    print ''
+
+    j = getRowBySA(fm, index-1, a, alphabet)
+    j2 = LF(fm, newRow)
+    while not j == j2:
+        newJ = LF(fm, j)
+        fm = moveRow(fm, j, j2)
+        j = newJ
+        j2 = LF(fm, j2)
+
+    return fm 
+
+def LF(fm, index):
+    ''' Step forward one step in the bwt and return the next row '''
+    (first, last, sa, checkpoints) = fm
+
+    c = last[index]
+    return (first[c][0] + getCount(last, index, checkpoints, b, sorted(first.keys()))) % len(last)
+
+length = 10
+t = ['$']
+for i in range(length):
+    t = [random.choice(['A', 'C', 'T', 'G'])] + t
+print t
+
+alphabet = sorted(set(t))
+a = 3
+b = 3
+last, sa = bwtViaBwm(t, a)
+_, tots = rankBwt(last)
+first = firstCol(tots)
+checkpoints = getCheckpoints(last, b, alphabet)
+fm = (first, last, sa, checkpoints)
+
+
+for i in xrange(len(last)):
+    print last[i] + '\t' + str(getSAIndex(fm, i, a, alphabet))
+print '\n'
+
+insertId = random.randint(0,20)
+newChar = random.choice(['A', 'C', 'T', 'G'])
+print str(insertId) + ', ' + newChar
+t2 = t[:insertId] + [newChar] + t[insertId:]
+print t2
+
+last2, sa2 = bwtViaBwm(t2, a)
+_, tots2 = rankBwt(last2)
+first2 = firstCol(tots2)
+checkpoints2 = getCheckpoints(last2, b, alphabet)
+fm2 = (first2, last2, sa2, checkpoints2)
+
+fm_new = insert(fm, insertId, newChar)
+
+for i in xrange(len(last2)):
+    print last2[i] + '\t' + str(getSAIndex(fm2, i, a, alphabet)) + '\t' + fm_new[1][i] + '\t' + str(getSAIndex(fm_new, i, a, alphabet))
+
+
+
+'''
 t = 'TTAGCCAATGGAATGGAAGCCGAT$'
 query1 = 'AGCC'
 query2 = 'AAT'
-query3 = 'ACGTA'
+query3 = 'ACTGG'
 alphabet = sorted(set(t))
 
 a = 3
@@ -169,3 +365,55 @@ print 'Searching for: ' + query3
 matches = find(fm, a, b, query3)
 print matches
 
+
+
+t2 = 'TTAGCCAACTGGAATGGAAGCCGAT$'
+
+last2, sa2 = bwtViaBwm(t2, a)
+_, tots2 = rankBwt(last2)
+first2 = firstCol(tots2)
+checkpoints2 = getCheckpoints(last2, b, alphabet)
+fm2 = (first2, last2, sa2, checkpoints2)
+
+print 'T = ' + t2
+print 'Searching for: ' + query1
+matches = find(fm2, a, b, query1)
+print matches
+
+print 'Searching for: ' + query2
+matches = find(fm2, a, b, query2)
+print matches
+
+print 'Searching for: ' + query3
+matches = find(fm2, a, b, query3)
+print matches
+
+
+print '\nUpdating initial index'
+fm_updated = insert(fm, 8, 'C')
+print 'T = ' + t2
+print 'Searching for: ' + query1
+matches = find(fm_updated, a, b, query1)
+print matches
+
+print 'Searching for: ' + query2
+matches = find(fm_updated, a, b, query2)
+print matches
+
+print 'Searching for: ' + query3
+matches = find(fm_updated, a, b, query3)
+print matches
+
+
+for i in xrange(len(last)):
+    for c in alphabet:
+        if i >= first[c][0] and i < first[c][1]:
+            print c + '\t' + last[i] + '\t' + str(getSAIndex(fm,i,a,alphabet)) + '\t' + str(getCount(last, i, checkpoints, b, alphabet))
+
+print '\n'
+(first, last, _, checkpoints) = fm2
+for i in xrange(len(last)):
+    for c in alphabet:
+        if i >= first[c][0] and i < first[c][1]:
+            print c + '\t' + last[i] + '\t' + str(getSAIndex(fm2, i, a, alphabet)) + '\t' + str(getCount(last, i, checkpoints, b, alphabet))
+'''
